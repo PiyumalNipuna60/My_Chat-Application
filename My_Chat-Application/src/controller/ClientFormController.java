3c9addac08f6aa35589d0a845fa13fc6cef6d721
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-01 23:04:31 +0530

    Add model packege and Client"

--- My_Chat-Application/src/controller/ClientFormController.java
+++ My_Chat-Application/src/controller/ClientFormController.java
@@ -5,9 +5,18 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class ClientFormController {
     public ScrollPane main;
     public VBox vbox_msg;
     public TextField txtMsg;
     public Button btnSend;
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
