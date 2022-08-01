39de4b00ebaea1cda501831890e5134ed5865c2c
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-01 23:00:34 +0530

    Add AppInitialize Server

---
+++ My_Chat-Application/src/ServerAppInitializer.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ServerAppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane pane = FXMLLoader.load(this.getClass().getResource("view/ServerForm.fxml"));
+        Scene scene= new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }}
