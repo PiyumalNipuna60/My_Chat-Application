fed28ad3de42011847f7cb19340f3c9cdc1d19df
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-01 23:22:07 +0530

    Add initialize Server and crete ServerSocket

--- My_Chat-Application/src/controller/ServerFormController.java
+++ My_Chat-Application/src/controller/ServerFormController.java
@@ -5,11 +5,26 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
+import model.Server;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class ServerFormController {
     public ScrollPane main;
     public VBox vbox_msg;
     public TextField txtMsg;
     public Button btnSend;
+    public Server server;
 
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            server = new Server(new ServerSocket(8000));
+            System.out.println("Connected to Sever.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
 }
--- My_Chat-Application/src/model/Server.java
+++ My_Chat-Application/src/model/Server.java
@@ -1,6 +1,28 @@
 package model;
 
-public class Server {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public Server(ServerSocket serverSocket) {
+        try {
+            this.serverSocket = serverSocket;
+            this.socket = serverSocket.accept();
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error creating server.");
+
+        }
+    }
 }
 
